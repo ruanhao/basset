9c56c9a9bc46e226ecd62272c606b0263d0facee
ruanhao <ruanhao1116@gmail.com>
2014-09-03 22:27:59 +0800

    io update

--- c/io.c
+++ c/io.c
@@ -5,95 +5,130 @@
 #define	STDERR_FILENO	2	/* Standard error output.  */
 
 
+
+
+
+/* ==================== Open File ==================== */
+
 #include <fcntl.h>
-/* Returns: file descriptor if OK, –1 on error */
 int open(const char *pathname, int oflag, ... /* mode_t mode */ );
+/* Returns: file descriptor if OK, -1 on error */
+/* #define RWRWRW (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
+   open("path/to/file", O_WRONLY | O_CREAT | O_TRUNC, RWRWRW); */
+
 /*
-oflag argument is formed by ORing together one or more of the following
-constants from the <fcntl.h> header:
+ * oflag argument is formed by ORing together one or more of the following
+ * constants from the <fcntl.h> header:
+ *
+ * O_RDONLY   Open for reading only.
+ * O_WRONLY   Open for writing only.
+ * O_RDWR     Open for reading and writing
+ *
+ * The following constants are optional:
+ *
+ * O_APPEND   Append to the end of file on each write
+ * O_CREAT    Create the file if it doesn't exist.
+ *            This option requires a third argument to the open function, the mode,
+ *            which specifies the access permission bits ofthe new file
+ * O_EXCL     Generate an error if O_CREAT is also specified and the file already exists.
+ *            This test for whether the file already exists and the creation of
+ *            the file if it doesn't exist is an atomic operation
+ * O_TRUNC    If the file exists and if it is successfully opened for
+ *            either write-only or read–write, truncate its length to 0
+ * O_NOCTTY   If the pathname refers to a terminal device, do not allocate
+ *            the device as the controlling terminal for this process
+ * O_NONBLOCK If the pathname refers to a FIFO, a block special file,
+ *            or a character special file, this option sets the nonblocking mode
+ *            for both the opening of the file and subsequent I/O
+ *
+ * The following three flags are also optional.
+ * They are part of the synchronized input and output option of the
+ * Single UNIX Specification (and thus POSIX.1):
+ *
+ * O_DSYNC    Have each write wait for physical I/O to complete,
+ *            but don't wait for file attributes to be updated if
+ *            they don't affect the ability to read the data just written
+ * O_RSYNC    Have each read operation on the file descriptor wait until
+ *            any pending writes for the same portion of the file are complete
+ * O_SYNC     Have each write wait for physical I/O to complete,
+ *            including I/O necessary to update file attributes modified as a result of the write
+ */
 
-O_RDONLY   Open for reading only.
-O_WRONLY   Open for writing only.
-O_RDWR     Open for reading and writing
-
-The following constants are optional:
-
-O_APPEND   Append to the end of file on each write
-O_CREAT    Create the file if it doesn't exist. This option requires a third argument to the open function, the
-           mode, which specifies the access permission bits ofthe new file
-O_EXCL     Generate an error if O_CREAT is also specified and the file already exists. This test for whether the
-           file already exists and the creation of the file ifit doesn't exist is an atomic operation
-O_TRUNC    If the file exists and if it is successfully opened for either write-only or read–write, truncate its
-           length to 0
-O_NOCTTY   If the pathname refers to a terminal device, do not allocate the device as the controlling terminal
-           for this process
-O_NONBLOCK If the pathname refers to a FIFO, a block special file, or a character special file, this option sets
-           the nonblocking mode for both the opening of the file and subsequent I/O
-
-The following three flags are also optional. They are part of the synchronized input and output optionof the
-Single UNIX Specification (and thus POSIX.1):
-
-O_DSYNC    Have each write wait for physical I/O to complete, but don't wait for file attributes to be updated if
-           they don't affect the ability to read the data just written
-O_RSYNC    Have each read operation on the file descriptor wait until any pending writes for the same portion of
-           the file are complete
-O_SYNC     Have each write wait for physical I/O to complete, including I/O necessary to update file attributes
-           modified as a result of the write
-*/
-// Eg: open(pathname, O_WRONLY | O_CREAT | O_TRUNC, mode);
 
 #include <unistd.h>
-/* Returns: 0 if OK, –1 on error */
 int close(int filedes);
+/* Returns: 0 if OK, -1 on error */
+
+
+
+
+/* ==================== read & write System Call ==================== */
 
 #include <unistd.h>
-/* Returns: new file offset if OK, –1 on error */
-off_t lseek(int filedes, off_t offset, int whence); /* If whence is SEEK_SET, the file's offset is set to offset bytes from thebeginning of the file
-                                                     * If whence is SEEK_CUR, the file's offset is set to its current value plus the offset
-                                                       The offset can be positive or negative
-                                                     * If whence is SEEK_END, the file's offset is set to the size of the file plus the offset
-                                                       The offset can be positive or negative
-                                                     */
-
-#include <unistd.h>
-/* Returns: number of bytes read, 0 if end of file, –1 on error */
 ssize_t read(int filedes, void *buf, size_t nbytes);
+/* Returns: number of bytes read, 0 if end of file, -1 on error */
 
 #include <unistd.h>
-/* Returns: number of bytes written if OK, –1 on error */
 ssize_t write(int filedes, const void *buf, size_t nbytes);
+/* Returns: number of bytes written if OK, -1 on error */
+
+
+
+
+
+/* ==================== Atomically read & write  ==================== */
 
 #include <unistd.h>
-/* The Single UNIX Specification includes XSI extensions that allow applications to seek and perform I/O ATOMICALLY
-   These extensions are pread and pwrite: */
-/* Returns: number of bytes read, 0 if end of file, –1 on error */
+/* pread and pwrite allow applications to seek and perform I/O ATOMICALLY */
 ssize_t pread(int filedes, void *buf, size_t nbytes, off_t offset);
-/* Returns: number of bytes written if OK, –1 on error */
+/* Returns: number of bytes read, 0 if end of file, -1 on error */
 ssize_t pwrite(int filedes, const void *buf, size_t nbytes, off_t offset);
+/* Returns: number of bytes written if OK, -1 on error */
+
+
+
+
+/* ==================== Misc ==================== */
 
 #include <unistd.h>
-/* Both return: new file descriptor if OK, –1 on error */
 int dup(int filedes);
 int dup2(int filedes, int filedes2);
+/* Both return: new file descriptor if OK, -1 on error */
 
 #include <unistd.h>
-/* Returns: 0 if OK, –1 on error */
 int fsync(int filedes);
 int fdatasync(int filedes);
-/* No return */
+/* Returns: 0 if OK, -1 on error */
 void sync(void);
 
 #include <fcntl.h>
+int fcntl(int filedes, int cmd, ... /* int arg */ );
 /* Returns: depends on cmd if OK, –1 on error */
-int fcntl(int filedes, int cmd, ... /* int arg */ ); /*
-                                                       The fcntl function is used for five different purposes:
-                                                       1.  Duplicate an existing descriptor (cmd = F_DUPFD)
-                                                       2.  Get/set file descriptor flags (cmd = F_GETFD or F_SETFD)
-                                                       3.  Get/set file status flags (cmd = F_GETFL or F_SETFL)
-                                                       4.  Get/set asynchronous I/O ownership (cmd = F_GETOWN or F_SETOWN)
-                                                       5.  Get/set record locks (cmd = F_GETLK, F_SETLK, or F_SETLKW)
-                                                     */
+/*
+ *  The fcntl function is used for five different purposes:
+ *   1.  Duplicate an existing descriptor   (cmd = F_DUPFD)
+ *   2.  Get/set file descriptor flags      (cmd = F_GETFD or F_SETFD)
+ *   3.  Get/set file status flags          (cmd = F_GETFL or F_SETFL)
+ *   4.  Get/set asynchronous I/O ownership (cmd = F_GETOWN or F_SETOWN)
+ *   5.  Get/set record locks               (cmd = F_GETLK, F_SETLK, or F_SETLKW)
+ */
+
 
 #include <sys/ioctl.h> /* BSD and Linux */
-/* Returns: –1 on error, something else if OK */
 int ioctl(int filedes, int request, ...);
+/* Returns: –1 on error, something else if OK */
+
+#include <unistd.h>
+off_t lseek(int filedes, off_t offset, int whence);
+/* Returns: new file offset if OK, -1 on error */
+/*
+ *   whence   |  description
+ *   ---------|--------------------------------------------------------------------
+ *   SEEK_SET |  file's offset is set to offset bytes from thebeginning of the file
+ *   ---------|--------------------------------------------------------------------
+ *   SEEK_CUR |  file's offset is set to its current value plus the offset
+ *            |  offset can be positive or negative
+ *   ---------|--------------------------------------------------------------------
+ *   SEEK_END |  file's offset is set to the size of the file plus the offset
+ *            |  offset can be positive or negative
+ */
--- c/stdio.c
+++ c/stdio.c
@@ -1,108 +1,160 @@
+/* ==================== Buffer ==================== */
+
 #include <stdio.h>
-/* Returns: 0 if OK, nonzero on error */
-void setbuf(FILE *restrict fp, char *restrict buf); /* To enable buffering, buf must point to a buffer of length BUFSIZ, a constant defined in <stdio.h>
-                                                       To disable buffering, we set buf to NULL
-                                                     */
+void setbuf(FILE *restrict fp, char *restrict buf);
+/* To enable buffering, buf must point to a buffer of length BUFSIZ, a constant defined in <stdio.h>
+ * To disable buffering, we set buf to NULL */
 int setvbuf(FILE *restrict fp, char *restrict buf, int mode, size_t size); /* mode :: [_IOFBF, _IOLBF, _IONBF] */
+/* Both Returns: 0 if OK, nonzero on error */
 
-/* Returns: 0 if OK, EOF on error */
-int fflush(FILE *fp); /* If fp is NULL, this function causes all output streams to be flushed */
 
-/* All three return: file pointer if OK, NULLon error */
+
+
+
+
+/* ==================== Open Stream ==================== */
+
 FILE *fopen(const char *restrict pathname, const char *restrict type);
-FILE *freopen(const char *restrict pathname, const char *restrict type, FILE *restrict fp); /* This function is typically used to open a specified file as one of the predefined streams:
-                                                                                               standard input, standard output, or standard error */
-FILE *fdopen(int filedes, const char *type); /* This function is often used with descriptors that are returned by the functions that create
-                                                pipes and network communication channels */
-
-/* The type argument for opening a standard I/O stream:
-   Type        Description
-   r/rb        open for reading
-   w/wb        truncate to 0 length or create for writing
-   a/ab        append; open for writing at end of file, or create for writing
-   r+/r+b/rb+  open for reading and writing
-   w+/w+b/wb+  truncate to 0 length or create for reading and writing
-   a+/a+b/ab+  open or create for reading and writing at end of file
-*/
+FILE *freopen(const char *restrict pathname, const char *restrict type, FILE *restrict fp);
+/* freopen is typically used to open a specified file as one of the predefined streams:
+ * standard input, standard output, or standard error */
+FILE *fdopen(int filedes, const char *type);
+/* fdopen is often used with descriptors that are returned by the functions that create
+ * pipes and network communication channels */
+/* All three return: file pointer if OK, NULLon error */
 
 /*
- * When a file is opened for reading and writing, the following restrictions apply:
- * 1. Output cannot be directly followed by input withoutan intervening fflush, fseek, fsetpos,or rewind
- * 2. Input cannot be directly followed by output withoutan intervening fseek, fsetpos,or rewind,
- *    or an input operation that encounters an end of file
-*/
+ * The type argument for opening a standard I/O stream:
+ * Type       |  Description
+ * -----------|----------------------------------------------------------------
+ * r/rb       |  open for reading
+ * -----------|----------------------------------------------------------------
+ * w/wb       |  truncate to 0 length or create for writing
+ * -----------|----------------------------------------------------------------
+ * a/ab       |  append; open for writing at end of file, or create for writing
+ * -----------|----------------------------------------------------------------
+ * r+/r+b/rb+ |  open for reading and writing
+ * -----------|----------------------------------------------------------------
+ * w+/w+b/wb+ |  truncate to 0 length or create for reading and writing
+ * -----------|----------------------------------------------------------------
+ * a+/a+b/ab+ |  open or create for reading and writing at end of file
+ */
 
 /* Returns: 0 if OK, EOF on error */
 int fclose(FILE *fp);
 
-/* Three functions allow us to read one character at atime */
-/* All three return: next character if OK, EOF on end of file or error */
-int getc(FILE *fp); /* can be implemented as a macro */
+int fflush(FILE *fp);
+/* if fp is NULL, this function causes all output streams to be flushed */
+/* Returns: 0 if OK, EOF on error */
+
+
+
+
+
+
+
+/* ==================== Read & Write Stream ==================== */
+
+/* read and write one character at a time */
+
+int getc(FILE *fp);  /* can be implemented as a macro */
 int fgetc(FILE *fp);
 int getchar(void);
+/* All three return: next character if OK, EOF on end of file or error */
 /* Note that these functions return the same value whether an error occurs or the end of file is reached.
-   To distinguish between the two, we must call either ferror or feof */
-/* Both return: nonzero (true) if condition is true, 0 (false) otherwise */
+ * To distinguish between the two, we must call either ferror or feof */
 int ferror(FILE *fp);
 int feof(FILE *fp);
+/* Both return: nonzero (true) if condition is true, 0 (false) otherwise */
 
 /* error flag and end-of-file flag can be cleared by calling clearerr */
 void clearerr(FILE *fp);
 
-/* Returns: c if OK, EOF on error */
 int ungetc(int c, FILE *fp);
+/* Returns: c if OK, EOF on error */
 
-
-/* All three return: c if OK, EOF on error */
-int putc(int c, FILE *fp); /* can be implemented as a macro */
+int putc(int c, FILE *fp);  /* can be implemented as a macro */
 int fputc(int c, FILE *fp);
 int putchar(int c);
+/* All three return: c if OK, EOF on error */
 
-/* Both return: buf if OK, NULL on end of file or error */
+
+
+
+/* read and write one line at a time */
+
 char *fgets(char *restrict buf, int n, FILE *restrict fp); /* reads up through the next newline */
 char *gets(char *buf);
+/* Both return: buf if OK, NULL on end of file or error */
 
-/* Both return: non-negative value if OK, EOF on error */
 int fputs(const char *restrict str, FILE *restrict fp); /* writes the null-terminated string to the specifiedstream */
 int puts(const char *str);
+/* Both return: non-negative value if OK, EOF on error */
 
-/* Both return: number of objects read or written */
+
+
+
+/* direct I/O */
+
 size_t fread(void *restrict ptr, size_t size, size_t nobj, FILE *restrict fp);
 size_t fwrite(const void *restrict ptr, size_t size, size_t nobj, FILE *restrict fp);
-/* Both fread and fwrite return the number of objects read or written.
- * For the read case, this number can be less than nobj if an error occurs or if the end of file is encountered.
- * In this case ferror or feof must be called.
- * For the write case, if the return value is less than the requested nobj, an error has occurred.
+/* Both return: number of objects read or written */
+/* both fread and fwrite return the number of objects read or written
+ * for read case:  this number can be less than nobj if an error occurs or if the end of file is encountered
+ *                 in this case ferror or feof must be called
+ * for write case: if the return value is less than the requested nobj, an error has occurred
  */
 
-/* Returns: current file position indicator if OK, –1L on error */
+
+
+
+
+
+
+
+/* ==================== Locate Stream ==================== */
+
 long ftell(FILE *fp);
-/* Returns: 0 if OK, nonzero on error */
+/* Returns: current file position indicator if OK, –1L on error */
 int fseek(FILE *fp, long offset, int whence); /* whence :: [SEEK_SET, SEEK_CUR, SEEK_END] */
+/* Returns: 0 if OK, nonzero on error */
 
 void rewind(FILE *fp);
 
-/* Both return: 0 if OK, nonzero on error */
 int fgetpos(FILE *restrict fp, fpos_t *restrict pos);
 int fsetpos(FILE *fp, const fpos_t *pos);
+/* Both return: 0 if OK, nonzero on error */
 
 
-/******************** Format I/O ****************************/
 
-/* Both return: number of characters output if OK, negative value if output error */
+
+
+
+
+
+/* ==================== I/O Format ==================== */
+
 int printf(const char *restrict format, ...);
 int fprintf(FILE *restrict fp, const char *restrictformat, ...);
+/* Both return: number of characters output if OK, negative value if output error */
 
-/* Both return: number of characters stored in array if OK, negative value if encoding error */
 int sprintf(char *restrict buf, const char *restrict format, ...);
 int snprintf(char *restrict buf, size_t n, const char *restrict format, ...);
+/* Both return: number of characters stored in array if OK, negative value if encoding error */
 
-/* All three return: number of input items assigned, EOF if input error or end of file before any conversion */
 int scanf(const char *restrict format, ...);
 int fscanf(FILE *restrict fp, const char *restrict format, ...);
 int sscanf(const char *restrict buf, const char *restrict format, ...);
+/* All three return: number of input items assigned, EOF if input error or end of file before any conversion */
 
-/*************** Details *******************/
 
-/* Returns: the file descriptor associated with the stream */
+
+
+
+
+
+
+/* ==================== Misc ==================== */
+
 int fileno(FILE *fp);
+/* Returns: the file descriptor associated with the stream */
