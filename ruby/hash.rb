a47b9e990505b9a03fe75f578d6c1f7f4d3b7d5e
ruanhao <ruanhao1116@gmail.com>
2014-12-14 19:54:02 +0800

    add ruby

---
+++ ruby/hash.rb
@@ -0,0 +1,7 @@
+chuck_norris = Hash[:punch, 99, :kick, 98, :stops_bullets_with_hands, true]
+
+a = [:punch, 0]
+b = [:kick, 72]
+c = [:stops_bullets_with_hands, false]
+key_value_pairs = [a, b, c]
+Hash[key_value_pairs]
